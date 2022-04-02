561dc7e6df77948ca12b290811848102559a5246
Skyleen77 <elsk8@outlook.fr>
2022-04-02 21:18:29 +0200

    add loader

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,12 @@ import MacOS from "./os/os.js";
 import Browser from "./navigateur/navigateur.js";
 import DinogameRender from "./navigateur/dinogame/dinogameRender.js";
 import ContactRender from "./contact/contactRender.js";
-import Contact from "./contact/contact.js"
+import Contact from "./contact/contact.js";
+
+window.addEventListener('load', () => {
+  document.body.classList.add('loaded');
+});
+
 
 MacOS();
 
